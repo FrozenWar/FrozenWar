79d98d507e6853de760e6527ad10eb3daa293e17
Duk Nam Yoo <yoo2001818@gmail.com>
2014-11-25 18:59:13 +0900

    Initial base structure of Java project

---
+++ src/main/java/com/frozenwar/FrozenWar.java
@@ -0,0 +1,10 @@
+package com.frozenwar;
+
+public class FrozenWar {
+
+  public static void main(String[] args) {
+    // TODO init code goes here...
+    System.out.println("Hello, world!");
+  }
+
+}
