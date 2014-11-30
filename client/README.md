6d238b36afbd1058394f5b39b0ebb97385548e7e
Duk Nam Yoo <yoo2001818@gmail.com>
2014-11-30 20:33:49 +0900

    Implemented basic code of expressjs

---
+++ client/README.md
@@ -0,0 +1,4 @@
+Code that is only used by client will come here.
+클라이언트만 사용하는 코드는 여기에 들어갑니다.
+
+Expected path: /js/
--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// TODO Server startup code comes here...
+var isProduction = process.env.NODE_ENV && process.env.NODE_ENV.trim().toLowerCase() == 'production';
+
+var express = require('express');
+var serveStatic = require('serve-static');
+
+var app = module.exports = express();
+
+app.use('/js', serveStatic(__dirname + '/client'));
+app.use('/js', serveStatic(__dirname + '/shared'));
+app.use(serveStatic(__dirname + '/public'));
+
+if (!module.parent) {
+    var server = app.listen(isProduction ? 80 : 3001, function(err) {
+        console.log('Listening on port %d', server.address().port);
+    });
+}
