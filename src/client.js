8509a700ce185472f8ae9995cc1bf07a3f368ead
Duknam Yoo <yoo2001818@gmail.com>
2015-07-24 01:07:23 +0900

    Implement basic code for socket.io

--- src/client.js
+++ src/client.js
@@ -1,4 +1,10 @@
 // Client init point
-import indexHtml from './assets/test.html';
+import io from 'socket.io-client';
+let socket = io();
 
+socket.on('connect', () => {
+  console.log('connected!');
+});
+
+import indexHtml from './assets/test.html';
 document.body.innerHTML = indexHtml;
--- src/server.js
+++ src/server.js
@@ -1,9 +1,20 @@
 // Server init point
 
 import express from 'express';
+import http from 'http';
+import socketIO from 'socket.io';
+import ServeStatic from 'serve-static';
 
 let app = express();
+let server = http.Server(app);
+let io = socketIO(server);
 
-app.listen(8000, () => {
+app.use(new ServeStatic('./dist'));
+
+server.listen(8000, () => {
   console.log('server started');
+});
+
+io.on('connection', (socket) => {
+  socket.emit('news', 'Hello, world!');
 });
