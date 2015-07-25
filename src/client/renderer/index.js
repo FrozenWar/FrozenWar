3c71f33ec3d2a9c86d73b29ce27b41aae9b425ea
Duknam Yoo <yoo2001818@gmail.com>
2015-07-25 21:50:12 +0900

    Implement viewport hexagon rendering

--- src/client/renderer/index.js
+++ src/client/renderer/index.js
@@ -1,13 +1,6 @@
 import PIXI from 'pixi.js';
 import {DebugTile} from './tile.js';
-
-function getRenderX(hexagon, x, y) {
-  return x * hexagon.width + (hexagon.width / 2 * (y & 1));
-}
-
-function getRenderY(hexagon, x, y) {
-  return y * (hexagon.height - hexagon.sideY - 2);
-}
+import Viewport from './viewport.js';
 
 let renderer;
 let stage = new PIXI.Container();
@@ -18,42 +11,11 @@ stage.addChild(container);
 
 let hexagon = DebugTile.hexagon;
 
-// Get hexagon tile size by width/height..
-function getHexWidth(width) {
-  return Math.ceil((width - hexagon.sideX - 2) / hexagon.width);
-}
-
-function getHexHeight(height) {
-  return Math.ceil((height - hexagon.sideY - 2) / (hexagon.height -
-    hexagon.sideY - 2));
-}
-
-let width = getHexWidth(800 / 2);
-let height = getHexHeight(600 / 2);
-
-let mapWidth = hexagon.width * width + hexagon.sideX + 2;
-let mapHeight = (hexagon.height - hexagon.sideY - 2) * height +
-  hexagon.sideY + 2;
-
-let mapBoundary = new PIXI.Graphics();
-mapBoundary.lineStyle(2, 0xFF0000, 1);
-mapBoundary.drawRect(0, 0, mapWidth, mapHeight);
-
-mapBoundary.lineStyle(2, 0x00FF00, 1);
-mapBoundary.drawRect(0, 0, 800 / 2, 600 / 2);
-
-for (let y = 0; y < height; ++y) {
-  for (let x = 0; x < width; ++x) {
-    var tile = new PIXI.Sprite(DebugTile.texture);
-    container.addChild(tile);
-    tile.position.x = getRenderX(DebugTile.hexagon, x, y);
-    tile.position.y = getRenderY(DebugTile.hexagon, x, y);
-  }
-}
-
-container.addChild(mapBoundary);
+let viewport = new Viewport(null, hexagon, 800, 600);
+container.addChild(viewport.container);
 
 export function render() {
+  viewport.moveCamera(1, 1);
   renderer.render(stage);
   requestAnimationFrame(render);
 }
---
+++ src/client/renderer/viewport.js
@@ -0,0 +1,104 @@
+import PIXI from 'pixi.js';
+// For debugging
+import {DebugTile} from './tile.js';
+
+export default class Viewport {
+  constructor(tilemap, hexagon, width, height) {
+    this.tilemap = tilemap;
+    this.width = width;
+    this.height = height;
+    this.hexagon = hexagon;
+    this.posX = 0;
+    this.posY = 0;
+    // We're using object to avoid making 'empty' reference
+    this.renderMap = {};
+    this.container = new PIXI.Container();
+    this.setCamera(0, 0);
+  }
+  getRenderX(x, y) {
+    return x * this.hexagon.width + (this.hexagon.width / 2 * (y & 1));
+  }
+  getRenderY(x, y) {
+    return y * (this.hexagon.height - this.hexagon.sideY - 2);
+  }
+  getRenderWidth(width) {
+    return Math.ceil((width - this.hexagon.sideX - 2) / this.hexagon.width);
+  }
+  getRenderHeight(height) {
+    return Math.ceil((height - this.hexagon.sideY - 2) / (this.hexagon.height -
+      this.hexagon.sideY - 2));
+  }
+  flooredModulo(x, y) {
+    // http://javascript.about.com/od/problemsolving/a/modulobug.htm
+    return ((x % y) + y) % y;
+  }
+  freeCamera(postX, postY) {
+    let stepWidth = this.hexagon.width;
+    let stepHeight = this.hexagon.height - this.hexagon.sideY - 2;
+    let tileX = Math.floor(this.posX / stepWidth) - 1;
+    let tileY = Math.floor(this.posY / stepHeight) - 1;
+    let tileWidth = this.getRenderWidth(this.width) + 1;
+    let tileHeight = this.getRenderHeight(this.height) + 1;
+    let endX = postX / stepWidth | 0;
+    let endY = postY / stepHeight | 0;
+    for (let y = 0; y <= tileHeight; ++y) {
+      let realY = tileY + y;
+      if (this.renderMap[realY] == null) continue;
+      let renderRow = this.renderMap[realY];
+      for (let x = 0; x <= tileWidth; ++x) {
+        let realX = tileX + x;
+        if (realX >= endX && realX < endX + tileWidth &&
+          realY >= endY && realY < endY + tileHeight) continue;
+        if (renderRow[realX] != null) {
+          let sprite = renderRow[realX];
+          this.container.removeChild(sprite);
+          delete renderRow[realX];
+        }
+      }
+    }
+  }
+  moveCamera(deltaX, deltaY) {
+    this.setCamera(this.posX + deltaX, this.posY + deltaY);
+  }
+  setCamera(posX, posY) {
+    this.freeCamera(posX, posY);
+    this.posX = posX;
+    this.posY = posY;
+    let stepWidth = this.hexagon.width;
+    let stepHeight = this.hexagon.height - this.hexagon.sideY - 2;
+    let tileX = Math.floor(this.posX / stepWidth) - 1;
+    let tileY = Math.floor(this.posY / stepHeight) - 1;
+    // Kinda tricky, it has to support negative numbers
+    let initX = -(this.flooredModulo(this.posX, stepWidth)) - stepWidth;
+    let initY = -(this.flooredModulo(this.posY, stepHeight)) - stepHeight;
+    let tileWidth = this.getRenderWidth(this.width) + 1;
+    let tileHeight = this.getRenderHeight(this.height) + 1;
+    for (let y = 0; y <= tileHeight; ++y) {
+      let realY = tileY + y;
+      let renderY = initY + stepHeight * y;
+      // Create empty object if undefined.
+      if (this.renderMap[realY] == null) {
+        this.renderMap[realY] = {};
+      }
+      let renderRow = this.renderMap[realY];
+      for (let x = 0; x <= tileWidth; ++x) {
+        let realX = tileX + x;
+        let renderX = initX + this.getRenderX(x, y + tileY);
+        // TODO validate
+        if (renderRow[realX] == null) {
+          // Create new sprite and add it to container.
+          let sprite = new PIXI.Sprite(DebugTile.texture);
+          this.container.addChild(sprite);
+          sprite.position.x = renderX;
+          sprite.position.y = renderY;
+          renderRow[realX] = sprite;
+        } else {
+          // Update position only
+          let sprite = renderRow[realX];
+          sprite.position.x = renderX;
+          sprite.position.y = renderY;
+        }
+      }
+    }
+  }
+}
