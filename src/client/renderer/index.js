aee5a3c17a8ef593f2fe405db08eb640aa52f2e5
Duknam Yoo <yoo2001818@gmail.com>
2015-07-25 15:09:11 +0900

    Implement determining hexagon map width and height

--- src/client/renderer/index.js
+++ src/client/renderer/index.js
@@ -10,14 +10,40 @@ function getRenderY(hexagon, x, y) {
 }
 
 let renderer;
-let stage = new PIXI.Graphics();
+let stage = new PIXI.Container();
 stage.interactive = true;
 
 let container = new PIXI.Container();
 stage.addChild(container);
 
-for (let y = 0; y < 10; ++y) {
-  for (let x = 0; x < 10; ++x) {
+let hexagon = DebugTile.hexagon;
+
+// Get hexagon tile size by width/height..
+function getHexWidth(width) {
+  return Math.ceil((width - hexagon.sideX - 2) / hexagon.width);
+}
+
+function getHexHeight(height) {
+  return Math.ceil((height - hexagon.sideY - 2) / (hexagon.height -
+    hexagon.sideY - 2));
+}
+
+let width = getHexWidth(800 / 2);
+let height = getHexHeight(600 / 2);
+
+let mapWidth = hexagon.width * width + hexagon.sideX + 2;
+let mapHeight = (hexagon.height - hexagon.sideY - 2) * height +
+  hexagon.sideY + 2;
+
+let mapBoundary = new PIXI.Graphics();
+mapBoundary.lineStyle(2, 0xFF0000, 1);
+mapBoundary.drawRect(0, 0, mapWidth, mapHeight);
+
+mapBoundary.lineStyle(2, 0x00FF00, 1);
+mapBoundary.drawRect(0, 0, 800 / 2, 600 / 2);
+
+for (let y = 0; y < height; ++y) {
+  for (let x = 0; x < width; ++x) {
     var tile = new PIXI.Sprite(DebugTile.texture);
     container.addChild(tile);
     tile.position.x = getRenderX(DebugTile.hexagon, x, y);
@@ -25,9 +51,9 @@ for (let y = 0; y < 10; ++y) {
   }
 }
 
+container.addChild(mapBoundary);
+
 export function render() {
-  container.x -= 2;
-  container.y -= 2;
   renderer.render(stage);
   requestAnimationFrame(render);
 }
