8412bbc80b03405ddd1778dfec13ce23b3a3a156
Duknam Yoo <yoo2001818@gmail.com>
2015-07-26 11:23:12 +0900

    Implement map dragging

--- src/client/renderer/index.js
+++ src/client/renderer/index.js
@@ -11,11 +11,45 @@ stage.addChild(container);
 
 let hexagon = DebugTile.hexagon;
 
-let viewport = new Viewport(null, hexagon, 800, 600);
+let viewport = new Viewport(null, hexagon, 400, 300);
 container.addChild(viewport.container);
+viewport.container.interactive = true;
+
+function onDragStart(event) {
+  this.data = event.data;
+  this.prevX = this.data.global.x;
+  this.prevY = this.data.global.y;
+  this.dragging = true;
+}
+
+function onDragEnd() {
+  this.dragging = false;
+  this.data = null;
+}
+
+function onDragMove() {
+  if (!this.dragging) return;
+  let diffX = this.data.global.x - this.prevX;
+  let diffY = this.data.global.y - this.prevY;
+  this.prevX = this.data.global.x;
+  this.prevY = this.data.global.y;
+  viewport.moveCamera(-diffX, -diffY);
+}
+
+viewport.container
+  // events for drag start
+  .on('mousedown', onDragStart)
+  .on('touchstart', onDragStart)
+  // events for drag end
+  .on('mouseup', onDragEnd)
+  .on('mouseupoutside', onDragEnd)
+  .on('touchend', onDragEnd)
+  .on('touchendoutside', onDragEnd)
+  // events for drag move
+  .on('mousemove', onDragMove)
+  .on('touchmove', onDragMove);
 
 export function render() {
-  viewport.moveCamera(1, 1);
   renderer.render(stage);
   requestAnimationFrame(render);
 }
